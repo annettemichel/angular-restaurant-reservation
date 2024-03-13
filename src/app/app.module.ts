18fa55622222763d3a26cbeb58c3a7131a3d289b
annmiche <annette.michel@hs-osnabrueck.de>
2024-03-13 19:48:19 +0100

    Created all modules, services and components, add routes, create reservation form with validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,21 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookComponent } from './book/book.component';
+import {FormsModule} from "@angular/forms";
+import {HomeModule} from "./home/home.module";
+import {ReservationModule} from "./reservation/reservation.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookComponent,
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HomeModule,
+    ReservationModule,
     AppRoutingModule
   ],
   providers: [
